7e367e5c14e8d4644212b2edc2b8ee78aba3ac18
何永能 <yongneng.he@dianping.com>
2017-06-08 10:50:00 +0800

    DevMode

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 
+const isDevMode = process.env.NODE_ENV !== 'production'
+Vue.config.debug = isDevMode
+Vue.config.devtools = isDevMode
+Vue.config.productionTip = isDevMode
+
 var app = new Vue({
   el: '#app',
   template: '<h2>{{ message }}</h2>',
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,8 +2,15 @@ const webpack = require('webpack')
 const config = require('./webpack.config.js')
 
 config.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+)
+
+config.plugins.push(
   new webpack.optimize.UglifyJsPlugin({
-    minimize: true,
     compress: {
       warnings: false
     }
