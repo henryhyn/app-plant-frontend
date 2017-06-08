b75e5551c7edad6120efa66229d11977130fa050
何永能 <yongneng.he@dianping.com>
2017-06-08 10:32:17 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
+const path = require('path');
+// 定义了一些文件夹的路径
+const ROOT_PATH = path.resolve(__dirname);
+const SRC_PATH = path.resolve(ROOT_PATH, 'src');
+const DIST_PATH = path.resolve(ROOT_PATH, 'dist');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   entry: {
-    app: './src/app.js'
+    app: '@/app.js'
   },
 
   output: {
-    filename: 'dist/app.js'
+    path: DIST_PATH,
+    filename: '[name].js'
   },
 
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': SRC_PATH
     }
   },
 
