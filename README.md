f21b99f85c5c1e13ea959abe0a816a1ba42fc17d
何永能 <yongneng.he@dianping.com>
2017-06-08 15:48:53 +0800

    more

---
+++ README.md
@@ -0,0 +1,13 @@
+# App Plant Frontend
+
+## 开发
+
+```
+npm start
+```
+
+## 发布
+
+```
+npm run build
+```
--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,4 +1,26 @@
 <template lang="jade">
   section
     h2 获取帮助
+
+    h3 基础
+    ul
+      li
+        a(href='https://vuejs.org' target='_blank') Core Docs
+      li
+        a(href='https://forum.vuejs.org' target='_blank') Forum
+      li
+        a(href='https://gitter.im/vuejs/vue' target='_blank') Gitter Chat
+      li
+        a(href='https://twitter.com/vuejs' target='_blank') Twitter
+
+    h3 生态
+    ul
+      li
+        a(href='http://router.vuejs.org' target='_blank') vue-router
+      li
+        a(href='http://vuex.vuejs.org' target='_blank') vuex
+      li
+        a(href='http://vue-loader.vuejs.org' target='_blank') vue-loader
+      li
+        a(href='https://github.com/vuejs/awesome-vue' target='_blank') awesome-vue
 </template>
