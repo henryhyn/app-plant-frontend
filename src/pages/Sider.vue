02669a172c9e954e82d7d292ab2d935768533404
何永能 <yongneng.he@dianping.com>
2017-06-10 22:42:00 +0800

    modify

--- src/pages/Sider.vue
+++ src/pages/Sider.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
-  aside: nav: el-menu(:router='true' :unique-opened='true' theme='dark')
-    el-menu-item(index='/') 首页
-    el-submenu(index='dishes')
-      template(slot='title') 资源管理
-      el-menu-item(index='/plants') 植物列表
-    el-submenu(index='commons')
-      template(slot='title') 常规信息
-      el-menu-item(index='/about') 关于
-      el-menu-item(index='/contact') 联系
-      el-menu-item(index='/help') 帮助
+  aside(class='el-menu--dark')
+    nav: el-menu(:router='true' :unique-opened='true' theme='dark')
+      el-menu-item(index='/') 首页
+      el-submenu(index='dishes')
+        template(slot='title') 资源管理
+        el-menu-item(index='/plants') 植物列表
+      el-submenu(index='commons')
+        template(slot='title') 常规信息
+        el-menu-item(index='/about') 关于
+        el-menu-item(index='/contact') 联系
+        el-menu-item(index='/help') 帮助
 </template>
