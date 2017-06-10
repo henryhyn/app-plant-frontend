9b4cb7191205e419fc45507a94a8ab015c93677e
何永能 <yongneng.he@dianping.com>
2017-06-10 22:48:34 +0800

    modify

--- src/pages/Sider.vue
+++ src/pages/Sider.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
-  aside(class='el-menu--dark')
+  aside.el-menu--dark
     nav: el-menu(:router='true' :unique-opened='true' theme='dark')
-      el-menu-item(index='/') 首页
+      el-menu-item(index='/') <i class='el-icon-date'></i> 首页
       el-submenu(index='dishes')
-        template(slot='title') 资源管理
-        el-menu-item(index='/plants') 植物列表
+        template(slot='title') <i class='el-icon-menu'></i> 资源管理
+        el-menu-item(index='/plants') <i class='el-icon-message'></i> 植物列表
       el-submenu(index='commons')
-        template(slot='title') 常规信息
-        el-menu-item(index='/about') 关于
-        el-menu-item(index='/contact') 联系
-        el-menu-item(index='/help') 帮助
+        template(slot='title') <i class='el-icon-setting'></i> 常规信息
+        el-menu-item(index='/about') <i class='el-icon-message'></i> 关于
+        el-menu-item(index='/contact') <i class='el-icon-message'></i> 联系
+        el-menu-item(index='/help') <i class='el-icon-message'></i> 帮助
 </template>
